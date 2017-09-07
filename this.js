f94bf151676396ef2fb1fd7ce01c181b45df37e3
Aaron Peyton <thealphabet101@gmail.co>
2017-09-06 17:08:49 -0700

    finished most of it...

--- this.js
+++ this.js
@@ -1,3 +1,58 @@
 $(document).ready(function() {
+  $("h3").css("visibility", "hidden");
 
+  $("#i0").click(function() {
+    $("#t0").css("visibility", "visible");
+  });
+  $("#i1").click(function() {
+    $("#t1").css("visibility", "visible");
+  });
+  $("#i2").click(function() {
+    $("#t2").css("visibility", "visible");
+  });
+  $("#i3").click(function() {
+    $("#t3").css("visibility", "visible");
+  });
+  $("#i4").click(function() {
+    $("#t4").css("visibility", "visible");
+  });
+  $("#i5").click(function() {
+    $("#t5").css("visibility", "visible");
+  });
+  $("#i6").click(function() {
+    $("#t6").css("visibility", "visible");
+  });
+  $("#i7").click(function() {
+    $("#t7").css("visibility", "visible");
+  });
+  $("#i8").click(function() {
+    $("#t8").css("visibility", "visible");
+  });
+  $("#i9").click(function() {
+    $("#t9").css("visibility", "visible");
+  });
+  $("#i10").click(function() {
+    $("#t10").css("visibility", "visible");
+  });
+  $("#i11").click(function() {
+    $("#t11").css("visibility", "visible");
+  });
+  $("#i12").click(function() {
+    $("#t12").css("visibility", "visible");
+  });
+  $("#i13").click(function() {
+    $("#t13").css("visibility", "visible");
+  });
+  $("#i14").click(function() {
+    $("#t14").css("visibility", "visible");
+  });
+  $("#i15").click(function() {
+    $("#t15").css("visibility", "visible");
+  });
+  $("#i16").click(function() {
+    $("#t16").css("visibility", "visible");
+  });
+  $("#i17").click(function() {
+    $("#t17").css("visibility", "visible");
+  });
 });
